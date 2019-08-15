274e604b3718836a285a4682e44935cd2f9fb998
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-15 12:13:27 -0700

    style Title component

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,20 +1,21 @@
 import React from 'react'
+import styles from './Title.module.css'
 
 
 export default function Title(props) {
     switch(props.size) {
         case "h1":
-            return <h1>{props.text}</h1>
+            return <h1 className={styles.title}>{props.text}</h1>
         case "h2":
-            return <h2>{props.text}</h2>
+            return <h2 className={styles.title}>{props.text}</h2>
         case "h3":
-            return <h3>{props.text}</h3>
+            return <h3 className={styles.title}>{props.text}</h3>
         case "h4":
-            return <h4>{props.text}</h4>
+            return <h4 className={styles.title}>{props.text}</h4>
         case "h5":
-            return <h5>{props.text}</h5>
+            return <h5 className={styles.title}>{props.text}</h5>
         case "h6":
-            return <h6>{props.text}</h6>
+            return <h6 className={styles.title}>{props.text}</h6>
     }
     
 }
